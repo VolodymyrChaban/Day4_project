fa3932db5e9b40fe0cbaec81b2ec3ec8c6df0bf0
VolodymyrChaban <vchabannn@gmail.com>
2021-05-25 19:23:42 -0400

    we heve done to shere to github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,3 +1,9 @@
 public class BranchingPractice {
 
+    public static void main(String[] args) {
+
+        //created new project
+        // enable version control
+        // added .gitignore with .idea
+    }
 }
